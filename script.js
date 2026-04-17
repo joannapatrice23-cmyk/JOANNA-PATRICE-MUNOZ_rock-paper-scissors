c729b861b62cd82c422b2b7bb4ef5419dd1e3831
joannapatrice23-cmyk <joannapatrice23@gmail.com>
2026-04-17 01:56:35 +0000

    adding another line for functionality

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 let playerScore = 0;
 let computerScore = 0;
+
+function playGame(playerChoice) {
+  const choices = ['rock', 'paper', 'scissors'];
+  const computerChoice = choices[Math.floor(Math.random() * 3)];
+
+  let result = '';
